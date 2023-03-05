5ff1085ef0965acdedb9da0a17e902b47f0e183f
Sumit Pandey <73009052+Sumit-Pandey-07@users.noreply.github.com>
2023-03-05 22:14:25 +0530

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+import { fetchPosts } from '../actions/posts';
+import { Home, Navbar } from './';
+
+const Login = () => <div>Login</div>;
+
+const Signup = () => <div>Signup</div>;
+
+class App extends React.Component {
+  componentDidMount() {
+    this.props.dispatch(fetchPosts());
+  }
+
+  render() {
+    const { posts } = this.props;
+    return (
+      <Router>
+        <div>
+          <Navbar />
+
+          <Route
+            exact
+            path="/"
+            render={() => {
+              return <Home posts={posts} />;
+            }}
+          />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    posts: state.posts,
+  };
+}
+
+App.propTypes = {
+  posts: PropTypes.array.isRequired,
+};
+
+export default connect(mapStateToProps)(App);
